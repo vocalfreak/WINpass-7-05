0e5edb8482d1c30391ec95b1e1c0fbdbaa0d3503
Syahirah <nursyahirah.aqilah.ainul@student.mmu.edu.my>
2025-04-22 17:57:13 +0800

    py file for camera

---
+++ photos.py
@@ -0,0 +1,32 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("Photobooth")
+
+img_counter = 0
+
+while True:
+    ret,frame = cam.read()
+
+    if not ret:
+        print("Failed to grab the frame")
+        break
+
+    cv2.imshow("test",frame)
+
+    k=cv2.waitKey(1)
+
+    if k%256 == 27:
+        print("Escape hit, closing the app")
+        break
+    
+    elif k%256 == 32:
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name,frame)
+        print("Photo taken")
+        img_counter+=1
+
+cam.release()
+
+cam.destroyAllWindows()
