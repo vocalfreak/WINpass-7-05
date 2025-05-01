1a39cd90863ce9da7d08c6d7fe392618477462d3
imantroopie <alyaiman06@gmail.com>
2025-05-01 14:17:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # from utils.route_utils import import_csv_init
-from flask import Flask, render_template, redirect, url_for, request, flash 
+from flask import Flask, render_template, redirect, url_for, request, flash, session 
 #from utils.image_utils import real_time_recognition
 import sqlite3
 from utils.route_utils import photobooth 
@@ -9,6 +9,7 @@ df_path = r"C:\Users\chiam\Downloads\Test_George.csv"
 db_path = r"C:\Users\chiam\Projects\WINpass-7-05\winpass.db"
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
 
 @app.route('/Landing-Page')
 def homepage():
@@ -137,6 +138,38 @@ def editing_page():
 def spend_invite():
     pass
 
+def check_credentials(ID, password):
+    conn = sqlite3.connect('winpass.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user WHERE ID = ? AND password = ?", (ID, password))
+    user = cursor.fetchone()
+    conn.close()
+    return user
+    
+@app.route('/')
+def home():
+    if 'ID' in session:
+        return render_template('home.html', ID=session['ID'])
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        ID = request.form['ID']
+        password = request.form['password'] 
+        
+        user = check_credentials(ID, password)
+        if user:
+            session['ID'] = ID
+            return redirect(url_for('home'))
+        else:
+            return "Incorrect ID or Password."
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('ID', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
