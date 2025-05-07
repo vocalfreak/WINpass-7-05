92f420bb1057b748cd6fd5d1f8d9e8a9671475b7
adriana <adriananurzahra@gmail.com>
2025-05-07 19:20:08 +0800

    fix: edited the style css for import btn and edit btn.

--- app.py
+++ app.py
@@ -247,8 +247,8 @@ if __name__ == '__main__':
 
     #Paths 
     df_path = r"C:\Users\chiam\Downloads\Test_George.csv"
-    db_path = r"C:\Users\chiam\Projects\WINpass-7-05\winpass.db"
-    image_folder_path = r"C:\Users\chiam\Projects\WINpass-7-05\winpass_training_set"
+    db_path = r"C:\Users\adria\Projects\WINpass-7-05\winpass.db"
+    image_folder_path = r"C:\Users\adria\Downloads\winpass_training_set"
 
     app.run(debug=True)
 
